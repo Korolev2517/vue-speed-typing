5ec3c9642ca318be914e2c94836a751e2c655435
Королев Константин <kkorolev@ntr.ai>
2025-10-25 18:22:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref, onMounted} from "vue";
+
+const text = ref('');
+const loading = ref(true)
+
+const fetchText = async () => {
+  try {
+    const response = await fetch('https://fish-text.ru/get');
+    const data = await response.json();
+    text.value = data.text;
+    console.log(text.value);
+  } catch (error) {
+    console.log('Error', error)
+  } finally {
+    loading.value = false;
+  }
+}
+
+onMounted(() => {
+  fetchText();
+})
+</script>
+
+<template>
+<div>
+  <p class="text">{{ text }}</p>
+</div>
+</template>
+
+<style scoped>
+.text {
+  font-size: 20px;
+}
+
+</style>
